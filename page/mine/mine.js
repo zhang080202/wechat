566ad68c84f8bb06ca1c27fb0acd65d55a616887
zhangyunfei <921233440@qq.com>
2018-11-14 18:12:09 +0800

    import iview

--- page/mine/mine.js
+++ page/mine/mine.js
@@ -15,13 +15,26 @@ Page({
     list: [],
     status: 0,
     showView: false,
-    cid: 0
+    cid: 0,
+    visible1: false, //提交审核Modal
+    visible: false,// Modal 是否显示
+    actions: [
+        {
+          name: '预览'
+        },
+        {
+          name: '编辑'
+        },
+        {
+          name: '提交审核'
+        }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     var that = this;
 
     /**
@@ -29,7 +42,7 @@ Page({
      */
     wx.getSystemInfo({
 
-      success: function(res) {
+      success: function (res) {
         that.setData({
           winWidth: res.windowWidth,
           winHeight: res.windowHeight
@@ -70,7 +83,7 @@ Page({
   /**
    * 滑动切换tab
    */
-  bindChange: function(e) {
+  bindChange: function (e) {
 
     var that = this;
     that.setData({
@@ -81,7 +94,7 @@ Page({
   /**
    * 点击tab切换
    */
-  swichNav: function(e) {
+  swichNav: function (e) {
 
     var that = this;
 
@@ -97,7 +110,7 @@ Page({
   /**
    * 操作点击事件
    */
-  clickOp: function(e) {
+  clickOp: function (e) {
     console.log("clicl event ", e.currentTarget.dataset.id);
     var that = this;
     if (that.data.cid == e.currentTarget.dataset.id) {
@@ -114,51 +127,128 @@ Page({
   },
 
   /**
+   * 点击item 弹出modal事件
+   */
+  clickItem: function(e) {
+    this.setData({
+      visible: true,
+      cid: e.currentTarget.dataset.id
+    })
+  },
+
+  /**
+   * 点击Modal 中的按钮
+   */
+  handleClick: function(e) {
+    console.log("Modal 返回值 ", e);
+    const op = e.detail.index;
+    // 预览
+    if(op == 0) {
+      wx.navigateTo({
+        url: '../arcticle/detail/detail?articleId=' + this.data.cid,
+      })
+    }
+    //编辑
+    if(op == 1) {
+
+    }
+    //提交审核
+    if(op == 2) {
+      this.setData({
+        visible1: true
+      })
+    }
+    //取消Modal
+    if(op == 3) {
+      this.setData({
+        visible: false
+      })
+    }
+  },
+
+  /**
+   * 关闭提交审核Modal
+   */
+  handleClose1: function(e) {
+    this.setData({
+      visible1: false
+    })
+  },
+
+  handleClose: function (e) {
+    this.setData({
+      visible: false
+    })
+  },
+
+  /**
+   * 提交审核
+   */
+  handleSubmit: function(e) {
+    wx.showLoading({
+      title: '提交中'
+    })
+    wx.request({
+      url: '',
+      method: "GET",
+      success: res => {
+
+      },
+      fail: res => {
+        wx.showToast({
+          title: "网络异常，请稍后再试",
+          icon: "none"
+        })
+      }
+    })
+  },
+
+  /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
